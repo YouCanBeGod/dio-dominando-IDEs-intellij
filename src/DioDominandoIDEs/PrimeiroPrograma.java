48f816a9531dc5844c0988e507e04d8ac36ac795
YouCanBeGod <dallasta.amos@gmail.com>
2022-10-23 20:02:06 -0300

    first commit

---
+++ src/DioDominandoIDEs/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package DioDominandoIDEs;
+
+import DioDominandoIDEs.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("lila", "marrom", 2);
+
+        System.out.println(gato);
+
+    }
+}
